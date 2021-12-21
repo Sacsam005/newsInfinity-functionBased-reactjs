48c241b4afe0c7a7f44c919dfe5428a96b6eba9b
Sachin Samal <88958992+Sacsam005@users.noreply.github.com>
2021-12-21 15:06:41 -0600

    Add files via upload

---
+++ src/component/NewsItem.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const NewsItem = (props) => {
+  let {title, source, description, imageUrl, newsUrl, author, date} = props;
+
+  return (
+    <>
+      <div className="container my-3">
+        <div className="card">
+          <img
+            src={
+              !imageUrl
+                ? 'https://www.popsci.com/uploads/2020/03/05/DSDOU3CKQNAUPNV2H5PZPJ4D4E.jpg'
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="Images"
+          />
+          <div className="card-body ">
+            <h5 className="card-title">
+              {title}...
+              <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                {source}
+              </span>
+            </h5>
+
+            <p className="card-text">{description}...</p>
+            <p className="card-text">
+              <small className="text-muted">{source}</small>
+            </p>
+            <p className="card-text">
+              <small className="text-muted">
+                By <b>{author === null ? 'Unknown' : author}</b> on{' '}
+                {new Date(date).toGMTString() > 12 ? 12 - date : date}
+              </small>
+            </p>
+          </div>
+        </div>
+        <a href={newsUrl} target="_blank" className="btn btn-sm btn btn-info my-2">
+          Read more &rarr;
+        </a>
+      </div>
+    </>
+  );
+};
+
+export default NewsItem;
